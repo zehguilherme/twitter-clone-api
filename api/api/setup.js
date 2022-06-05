33d8454fe7687d0081fcbee87aa0e1bcaaa5cdbc
José Guilherme <jgtomaine@hotmail.com>
2022-06-05 14:28:43 -0300

    Adição da API

---
+++ api/api/setup.js
@@ -0,0 +1,12 @@
+import cors from '@koa/cors'
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+
+import { router } from './routes.js'
+
+export const app = new Koa()
+
+app.use(cors())
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
